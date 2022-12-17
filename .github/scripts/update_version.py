2f3987aa769098200fe2ba647fd829a921a7e4e1
Le Tuan Vu - DS <vult@galaxy.one>
2022-12-17 19:46:10 +0700

    fix ci

--- .github/scripts/update_version.py
+++ .github/scripts/update_version.py
@@ -1,6 +1,5 @@
 import os
 import argparse
-from pathlib import Path
 
 parser = argparse.ArgumentParser(description='UPDATE VERSION')
 parser.add_argument('--version', type=str, default='0.0.1')
@@ -8,4 +7,6 @@ args = parser.parse_args()
 args.version = str(args.version.split('/')[-1])
 print('__VERSION__: {}'.format(args.version))
 
-(Path(__file__).parent / "__VERSION__").write_text(str(args.version))
+with open(os.path.join(os.path.abspath(os.path.join(__file__, os.pardir)), "__VERSION__"), "w") as f:
+    f.write(str(args.version))
+
